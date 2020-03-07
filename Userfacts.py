11ce6825aa8e17e2e0265ac0965c3257c1e30e74
Psycho <laurentchervet@bluewin.ch>
2020-03-07 22:16:59 +0100

    Getting think up

--- Userfacts.py
+++ Userfacts.py
@@ -1,6 +1,6 @@
 from core.base.model.AliceSkill import AliceSkill
+from core.base.model.Intent import Intent
 from core.dialog.model.DialogSession import DialogSession
-from core.util.Decorators import IntentHandler
 
 
 class Userfacts(AliceSkill):
@@ -9,6 +9,46 @@ class Userfacts(AliceSkill):
 	Description: Now alice can remember things you teach her!
 	"""
 
-	@IntentHandler('MyIntentName')
-	def dummyIntent(self, session: DialogSession, **_kwargs):
-		pass
+	_INTENT_GET_USER_FACT    = Intent('GetUserFact')
+	_INTENT_DELETE_ALL_FACTS = Intent('DeleteAllUserFacts')
+	_INTENT_USER_ANSWER      = Intent('UserRandomAnswer')
+	_INTENT_ANSWER_YES_OR_NO = Intent('AnswerYesOrNo')
+
+	DATABASE = {
+		'facts': [
+			'id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE',
+			'username TEXT NOT NULL',
+			'fact TEXT NOT NULL',
+			'value TEXT NOT NULL'
+		]
+	}
+
+	def __init__(self):
+		self._INTENTS = [
+			(self._INTENT_GET_USER_FACT, self.getUserFact)
+		]
+
+		super().__init__(supportedIntents=self._INTENTS, databaseSchema=self.DATABASE)
+
+
+	def getUserFact(self, session: DialogSession, **_kwargs):
+		if not session.user:
+			self.endDialog(sessionId=session.sessionId, text=self.randomTalk(text='dontKnowYou'))
+
+		answer = self.databaseFetch(
+			tableName='facts',
+			query='SELECT * FROM :__table__ WHERE username=`{username}` AND fact=`{fact}`',
+			values={
+				'username': session.user,
+				'fact': session.slotRawValue('Userfact')
+			}
+		)
+
+		if not answer:
+			self.continueDialog(
+				sessionId=session.sessionId,
+				text=self.randomTalk(text='noResult'),
+				intentFilter=[self._INTENT_USER_ANSWER],
+				probabilityThreshold=0.1,
+				currentDialogState='answeringFactValue'
+			)
